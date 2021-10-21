1bd3ee66ad9bb2e1aaaed677a1ddd536a7c837f0
Matthew Lindfield Seager <matthew@sagacity.com.au>
2021-10-21 23:58:59 +0700

    Fix Static Controller Test
    I'm sure it was passing before... which makes no sense looking at it 
    now!

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -7,7 +7,6 @@ class StaticControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "missing template raises a routing error (which becomes a 404 later)" do
-    get "/page/this-is-a-made-up-url"
-    assert_raises(ActionController::RoutingError)
+    assert_raises(ActionController::RoutingError) { get "/page/this-is-a-made-up-url" }
   end
 end
