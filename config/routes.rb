20047b150190ded1b8b27b14918dabce12887de2
Matthew Lindfield Seager <matthew@sagacity.com.au>
2021-11-07 13:50:38 +0700

    Minor tidy up/prettification

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   end
 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
-  #Override SessionsController to change URL after sign out
+  # Override SessionsController to change URL after sign out on #destroy
   delete "/sign_out" => "sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
   get "/confirm_email/:token" => "email_confirmations#update", as: "confirm_email"
