39737184ce1593af3f2761169ddeb1bdc925b446
Matthew Lindfield Seager <matthew@sagacity.com.au>
2022-09-19 15:27:43 +0000

    Add UGLY user creation logic to group controller
    Unrelated but I'm just stuffing it in here for now to prove the concept.
    
    Need to make all of this stuff asynchronous at some point

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,6 +9,7 @@ class GroupsController < ApplicationController
   end
   
   def update_members
+    num_people_created = 0
     num_people_skipped = 0
     num_members_added = 0
     gmf = Google::GroupMembersFetcher.call(group_key: @group.dir_email)
@@ -22,9 +23,21 @@ class GroupsController < ApplicationController
         next unless m[:type] == "USER"
         person = Person.find_by(school_email: m[:email]) || Person.find_by(personal_email: m[:email])
         if !person
-          Rails.logger.warn("Group member skipped, person not found - #{m[:email]}")
-          num_people_skipped += 1
-          next
+          uf = Google::UserFetcher.call(user_key: m[:email])
+          num_people_skipped += 1 && next unless uf.payload.present?
+          person = Person.create!(
+            school_email: uf.payload[:primaryEmail],
+            official_given_name: uf.payload[:name][:givenName],
+            official_family_name: uf.payload[:name][:familyName],
+          )
+          dr = DirectoryRecord.find_or_create_by!(
+            person: person,
+            directory_id: uf.payload[:id],
+            email: uf.payload[:primaryEmail],
+            email_aliases: uf.payload[:aliases] || [],
+            org_unit: uf.payload[:orgUnitPath]
+          )
+          num_people_created += 1
         end
         membership = DirectoryGroupMembership.find_or_initialize_by(group: @group, person: person)
         if !membership.persisted?
@@ -34,7 +47,7 @@ class GroupsController < ApplicationController
       end
     end
     
-    redirect_to @group, notice: "#{t('.people_skipped', count: num_people_skipped) if num_people_skipped > 0} #{t('.members_added', count: num_members_added)}"
+    redirect_to @group, notice: "#{t('.people_skipped', count: num_people_skipped) if num_people_skipped > 0} #{t('.people_created', count: num_people_created) if num_people_created > 0} #{t('.members_added', count: num_members_added)}"
   end
 
   private
