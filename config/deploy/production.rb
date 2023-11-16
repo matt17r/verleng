21aed995ceb57d93e03d7919f066f285f6e3131e
Matthew Lindfield Seager <matt@matt17r.com>
2023-11-16 16:15:12 +1100

    Update Capistrano deployment task
    Doesn't have to be an IP Address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,1 @@
-server ENV.fetch("CAPISTRANO_DEPLOYMENT_IP_ADDRESS"), user: "matthew", roles: %w{app db web}
+server ENV.fetch("CAPISTRANO_DEPLOYMENT_ADDRESS"), user: "matthew", roles: %w{app db web}
